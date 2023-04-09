95ea10c4cb895a2b4b82ca3318879099daac46ff
sergey-fomin <ya.sergeyfomin@gmail.com>
2023-04-09 20:26:14 +0500

    header refactor

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,49 +7,47 @@ type THeaderProps = {
     isLoggedIn: boolean;
 }
 
-export const Header = (props: THeaderProps) => {
-    switch(props.route) {
+const getLoginInfo = (route: string, isLoggedIn: boolean) => {
+    switch(route) {
         case 'registration':
             return (
-                <header className={classes.header}>
-                    <div className={classes.container}>
-                        <img className={classes.logo} src={mestoLogo}/>
-                        {/* <!-- Страница регистрации --> */}
-                        <div className={classes.loginInfo} data-route="registration">
-                            <a href="#" data-link="login" className={classes.link}>Войти</a>
-                        </div>
-                    </div>
-                </header>
+                <div className={classes.loginInfo}>
+                    <a href="#" className={classes.link}>Войти</a>
+                </div>
             );
         case 'login':
             return (
-                <header className={classes.header}>
-                    <div className={classes.container}>
-                        <img className={classes.logo} src={mestoLogo}/>
-                        {/* <!-- Страница логина --> */}
-                        <div className={classes.loginInfo} data-route="login">
-                            <a href="#" data-link="registration" className={classes.link}>Регистрация</a>
-                        </div>
-                    </div>
-                </header>
+                <div className={classes.loginInfo}>
+                    <a href="#" className={classes.link}>Регистрация</a>
+                </div>
             );
         default:
-            return (
-                <header className={classes.header}>
-                    <div className={classes.container}>
-                        <img className={classes.logo} src={mestoLogo}/>
-                        {/* <!-- Главная страница --> */}
-                        <div className={classes.loginInfo}>
-                            {/* <!-- Авторизованная зона --> */}
-                            <p data-authorized className={classes.email}></p>
-                            <a href="#" data-authorized data-link="logout" className={classes.link}>Выйти</a>
-                            {/* <!-- Неавторизованная зона --> */}
-                            <a href="#" data-unauthorized data-link="login" className={classes.link}>Войти</a>
-                            <span data-unauthorized>/</span>
-                            <a href="#" data-unauthorized data-link="registration" className={classes.link}>Регистрация</a>
-                        </div>
+            if (isLoggedIn) {
+                return (
+                    <div className={classes.loginInfo}>
+                        <p className={classes.email}></p>
+                        <a href="#" className={classes.link}>Выйти</a>
                     </div>
-                </header>
+                );
+            }
+
+            return (
+                <div className={classes.loginInfo} >
+                    <a href="#" className={classes.link}>Войти</a>
+                    <span>/</span>
+                    <a href="#" className={classes.link}>Регистрация</a>
+                </div>
             );
     }
+}
+
+export const Header = (props: THeaderProps) => {
+    return (
+        <header className={classes.header}>
+            <div className={classes.container}>
+                <img className={classes.logo} src={mestoLogo}/>
+                { getLoginInfo(props.route, props.isLoggedIn) }
+            </div>
+        </header>
+    );
 }
